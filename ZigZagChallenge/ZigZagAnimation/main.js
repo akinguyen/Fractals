c83d6e4dab884521c1ee17a00efa58292575f7fd
thaominh123 <duynguyenquykhanh@gmail.com>
2019-03-01 14:57:20 -0800

    add ZigZagAnimation

---
+++ ZigZagChallenge/ZigZagAnimation/main.js
@@ -0,0 +1,24 @@
+$(function() {
+  var zigZag = new ZigZag(new Position(16, 16), 30);
+  drawEngine(zigZag, zigZag.pathTraversal);
+  $("input[type='submit']").click(() => {
+    var i = parseInt($("input[name='i']").val());
+    var j = parseInt($("input[name='j']").val());
+    zigZag.delete();
+
+    zigZag = new ZigZag(new Position(i, j), 30);
+    drawEngine(zigZag, zigZag.pathTraversal);
+  });
+});
+
+//draw
+const drawEngine = (zigZag, path) => {
+  var i = 0;
+  var main = setInterval(() => {
+    if (i == path.length - 1) {
+      clearInterval(main);
+    }
+    zigZag.drawLine(path[i], path[i + 1]);
+    i++;
+  }, 100);
+};
